77eb3e3e9c276b5f9934a1d565b479768c26df79
Valerie <valeriealexarivera@gmail.com>
2025-08-08 13:27:53 -0400

    Initial Commit

---
+++ ToDoList2/ToDoList2App.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoList2App.swift
+//  ToDoList2
+//
+//  Created by Scholar on 8/8/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoList2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
