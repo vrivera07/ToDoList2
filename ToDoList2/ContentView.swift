2bb3d9a2c6a80a718bfdefef16ce2d0a44408df3
Valerie <valeriealexarivera@gmail.com>
2025-08-11 09:33:22 -0400

    debugged code

--- ToDoList2/ContentView.swift
+++ ToDoList2/ContentView.swift
@@ -1,6 +1,6 @@
 //
-// ContentView.swift
-// ToDoList45
+// NewToDo.swift
+// ToDoList
 //
 // Created by Scholar on 8/8/25.
 //
@@ -8,48 +8,45 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-    
-  @State private var showNewTask = false
-   @Query var toDos: [ToDoItem]
-  var body: some View {
-    VStack {
-      HStack{
-        Text("To-Do List")
-          .font(.system(size: 40))
-          .fontWeight(.bold)
-        Spacer()//push left or right
-        Button{
-          withAnimation{
-            showNewTask = true
-          }
-        } label: {
-          Text("+")
-            .font(.title)
-            .fontWeight(.bold)
-        }
-         
-      }//end of the HStack
-      Spacer()//push up
-       
-        List{
-            ForEach(toDos) { toDoItem in
+
+    @State private var showNewTask = false
+    @Query var toDos: [ToDoItem]
+      var body: some View {
+        VStack {
+          HStack{
+            Text("To Do List")
+              .font(.system(size: 40))
+              .fontWeight(.bold)
+            Spacer()//push left or right
+            Button{
+              withAnimation{
+                showNewTask = true
+              }
+            } label: {
+              Text("+")
+                .font(.title)
+                .fontWeight(.bold)
+            }
+          }//end of the HStack
+          .padding()
+          Spacer()//push up
+          List{//Displays list of tasks
+            ForEach (toDos) { toDoItem in
+              if toDoItem.isImportant {
+                Text("‼️" + toDoItem.title)
+              } else {
                 Text(toDoItem.title)
-                if toDoItem.isImportant {
-                    Text("‼️" + toDoItem.title)
-                } else{
-                    Text(toDoItem.title)
-                }
-            }// end forEach
-        }//end list
-        
-      if showNewTask {
-          NewToDo(showNewTask: $showNewTask, ToDoItem: ToDoItem(title: "", isImportant: false))
+              }
+            }
+          }
+          .listStyle(.plain)
+        }//end of the VStack
+        if showNewTask {
+          NewToDo(showNewTask: $showNewTask, toDoItem: ToDoItem(title: "", isImportant: false))
+        }
       }
-    }//end of the VStack
-    .padding()
-  }
-}
-#Preview {
-  ContentView()
-        .modelContainer
-}
+    }
+    #Preview {
+      ContentView()
+        .modelContainer(for: ToDoItem.self, inMemory: true)
+    }
--- ToDoList2/NewToDo.swift
+++ ToDoList2/NewToDo.swift
@@ -5,40 +5,40 @@
 //
 import SwiftUI
 struct NewToDo: View {
-   
+    
     @Binding var showNewTask: Bool
     @Bindable var toDoItem: ToDoItem
     @Environment(\.modelContext) var modelContext
     
-  var body: some View {
-    VStack {
-      Text("Task Title:")
-        .font(.title)
-        .fontWeight(.bold)
-        TextField("Enter the task description", text: $toDoItem.title)
+    var body: some View {
+        VStack {
+            Text("Task Title:")
+                .font(.title)
+                .fontWeight(.bold)
+            TextField("Enter the task description", text: $toDoItem.title)
+                .padding()
+                .background(Color(.systemGroupedBackground))
+                .cornerRadius(15)
+                .padding()
+            Toggle(isOn: $toDoItem.isImportant) {
+                Text("Is it important?")
+            }
+            Button {
+                addToDo()
+                showNewTask = false
+            } label: {
+                Text("Save")
+            }
+        }//end of VStack
         .padding()
-        .background(Color(.systemGroupedBackground))
-        .cornerRadius(15)
-        .padding()
-        Toggle(isOn: $toDoItem.isImportant) {
-        Text("Is it important?")
-      }
-      Button {
-          addToDo()
-          showNewTask = false
-      } label: {
-        Text("Save")
-      }
-    }//end of VStack
-    .padding()
-      
-      func addToDo() {
-          let toDo = ToDoList.ToDoItem(title: ToDoItem.title, isImportant: ToDoItem.isImportant)
-          modelContext.insert(toDo)
-      }
+    }
+        func addToDo() {
+            let toDo = ToDoItem(title: toDoItem.title, isImportant: toDoItem.isImportant)
+            modelContext.insert(toDo)
+        }
+    }
 
-  }
-}
 #Preview {
-    NewToDo(showNewTask:.constant (false),  ToDoItem: ToDoItem: "", isImportant: false)
-}
+        NewToDo(showNewTask: .constant(false), toDoItem: ToDoItem(title: "", isImportant: false))
+    }
+
